9dbceca00420263a7c8858139dc7f6df9956b6fc
jamesbowman <jamesb@excamera.com>
2013-12-15 19:08:27 -0800

    Checkpoint, VM is counting

--- src/memload.py
+++ src/memload.py
@@ -1,11 +1,12 @@
 import sys
 from ga144 import GA144
+import array
 
 class Memloader(GA144):
     def __init__(self, ramcontents):
         GA144.__init__(self)
         # self.poststream = ramcontents
-        ds = range(4000, 4026)
+        ds = array.array('H', open(sys.argv[2]).read()).tolist()
         self.poststream = [0x0ae, self.node['708'].symbols['SOUTH'], len(ds)] + ds
 
         self.loadprogram("memload.ga")
