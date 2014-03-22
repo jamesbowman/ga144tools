d5a8f679301e10515459e46d80f93a76f363db9e
James Bowman <jamesb@excamera.com>
2014-03-22 13:11:28 -0700

    Checkpoint for talk

--- src/draw.py
+++ src/draw.py
@@ -61,13 +61,37 @@ class Viz:
                     ctx.set_source_rgb(0, .0, .1)
                 self.cell(r, c, label)
 
-        if 0:
-            ctx.set_source_rgb(1, 1, 1)
-            ctx.move_to(*self.center(7, 8))
-            ctx.line_to(*self.center(7, 9))
+        if 1:
+            ctx.set_source_rgba(1, 1, 0, 0.5)
+            ctx.set_line_width(9)
+            ctx.move_to(*self.center(*self.path[0]))
+            for p in self.path:
+                ctx.line_to(*self.center(*p))
             ctx.stroke()
         self.surface.write_to_png(pngfile)
 
 if __name__ == '__main__':
     g = Viz()
+    path = ['SOUTH', 'SOUTH', 'SOUTH', 'SOUTH', 'SOUTH', 'SOUTH', 'SOUTH', 'WEST']
+    path = ['SOUTH', 'SOUTH', 'EAST', 'SOUTH', 'WEST', 'SOUTH', 'SOUTH', 'SOUTH', 'SOUTH', 'WEST']
+    s6w = ['SOUTH'] * 6 + ['WEST']
+    n6w = ['NORTH'] * 6 + ['WEST']
+    path = (['EAST'] * 9 + ['SOUTH'] + (s6w + n6w) * 8 +
+        s6w + ['NORTH'] * 7 + ['EAST'] * 7
+    )
+    (r, c) = (7, 8)
+    pa = [(r+1,c), (r,c)]
+    print path
+    for d in path:
+        if d == 'EAST':
+            c += 1
+        elif d == 'WEST':
+            c -= 1
+        elif d == 'NORTH':
+            r += 1
+        elif d == 'SOUTH':
+            r -= 1
+        pa.append((r,c))
+    print pa
+    g.path = pa
     g.render("out.png")
