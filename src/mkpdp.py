e31d796eb76108910244e614146b1579ee7a2efc
jamesbowman <jamesb@excamera.com>
2014-06-06 14:52:20 -0700

    Move VM to 508

--- src/mkpdp.py
+++ src/mkpdp.py
@@ -7,16 +7,14 @@ if __name__ == '__main__':
     g = GA144()
     g.loadprogram("pdp.ga")
 
-    # Find the symbols in 608 that are useful
-    publish = set(g.node['608'].symbols) - set(g.node['607'].symbols)
-    # Load them into 607:
-    for p,v in g.node['608'].symbols.items():
-        if p not in g.node['607'].symbols:
-            g.node['607'].symbols[p] = v
+    # Find the symbols in the core that are useful, load them
+    for p,v in g.node['508'].symbols.items():
+        if p not in g.node['507'].symbols:
+            g.node['507'].symbols[p] = v
 
     g.loadprogram("pdpfrag.ga")
 
-    pn = g.node['609']
+    pn = g.node['509']
     pn.prefix = [pn.assemble(["jump", "WEST"])]
     pn.load_pgm = array.array('H', open("binary").read()).tolist()
 
