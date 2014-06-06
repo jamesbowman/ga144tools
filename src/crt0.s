abd2e9beea209dc48a9cc40828a6c548b39cda4e
jamesbowman <jamesb@excamera.com>
2014-06-06 11:06:35 -0700

    CLR, COM, INC, DEC, NEG

--- src/crt0.s
+++ src/crt0.s
@@ -1,14 +1,22 @@
         .text
 
         .even
-        .word 0
-        .word 77
-        .word 0
-        .word 0
-        .word 0
-        .word 0
-        .word 0
+        .word 10000
+        .word 10001
+        .word 10002
+        .word 10003
+        .word 10004
+        .word 10005
+        .word 10006
         .word 16
 label:
+        neg r3
+        mfpi r0
         mfpi r1
+        mfpi r2
+        mfpi r3
+        mfpi r4
+        mfpi r5
+        mfpi r6
+        mfpi r7
         jmp label
