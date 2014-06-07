47a14a31ff2c2cf693b04a416ea4fe7258b19d1b
jamesbowman <jamesb@excamera.com>
2014-06-07 08:39:23 -0700

    File renames

--- src/pdp11/mkpdp.py
+++ src/pdp11/mkpdp.py
@@ -5,7 +5,7 @@ import draw
 
 if __name__ == '__main__':
     g = GA144()
-    g.loadprogram("pdp.ga")
+    g.loadprogram("vm.ga")
 
     # Find the symbols in the core that are useful, pre-load them
     # into the fragment nodes
@@ -14,7 +14,7 @@ if __name__ == '__main__':
             if p not in g.node[ucnode].symbols:
                 g.node[ucnode].symbols[p] = v
 
-    g.loadprogram("pdpfrag.ga")
+    g.loadprogram("fragments.ga")
 
     pn = g.node['509']
     pn.prefix = [pn.assemble(["jump", "WEST"])]
