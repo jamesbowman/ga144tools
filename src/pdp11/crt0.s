dee4ab42b2c977a4f8d1db07701065c075b7812b
jamesbowman <jamesb@excamera.com>
2014-06-07 16:00:08 -0700

    Comments cleanup

--- src/pdp11/crt0.s
+++ src/pdp11/crt0.s
@@ -3,13 +3,12 @@
 # The first 8 locations of memory are the registers R0-R7
 # This crt0 saves a few words by putting the initialization
 # code at 0 also - it is clobbered as soon as the real
-# program runs.
+# program uses R0-R5.
+
         .text
         .even
         mfpi    $0x5555         # Write 5555 to debug
         mfpi    $0xaaaa         # Write aaaa to debug
         jmp     _pdpmain        # That is main
-#        jmp     silly
-        .word 64                # Initial SP
+        .word 64                # Initial SP, above top of RAM
         .word 0                 # Initial PC
-
