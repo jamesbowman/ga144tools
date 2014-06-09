43f609864bf6c52c518be02cd72edbb7ebc36466
jamesbowman <jamesb@excamera.com>
2014-06-09 12:05:48 -0700

    Smoke test

--- src/pdp11/smoke.s
+++ src/pdp11/smoke.s
@@ -3,8 +3,19 @@
 
         .globl _pdpmain
 _pdpmain:
-        mov     $0x8017,r0
-        mfpi    r0
-        asl     r0
-        mfpi    r0
+        mov     $table,r1
+        mov     (r1)+,r0
+        asl     r0                      # Should be 80aa
+        add     (r1)+,r0                # Should be 845d
+        sub     (r1)+,r0                # Should be fd3c
+        add     @0(r1),r0               # Should be bd91
+        asr     r0                      # Should be dec8
+        sub     $(0xdec8+1),r0
+
+        mfpi    r0                      # Should be ffff
 hang:   br      hang
+
+table:  .word   0xc055
+        .word   947
+        .word   0x8721
+        .word   table
