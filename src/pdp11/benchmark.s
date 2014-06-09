4ac01040a42e4268eed756f45497560f6dcf04a4
jamesbowman <jamesb@excamera.com>
2014-06-09 11:51:07 -0700

    Benchmark comments

--- src/pdp11/benchmark.s
+++ src/pdp11/benchmark.s
@@ -1,11 +1,13 @@
+        .text
+        .even
+
         .globl _pdpmain
 _pdpmain:
         mov     $65535,r0
-        clr     r1
-        mfpi    $0x1111
+        mfpi    $0x1111                 # start timer
 delay:
         dec     r0
         cmp     $0,r0
         bne     delay
-        mfpi    $0x2222
+        mfpi    $0x2222                 # stop timer
         br      _pdpmain
