dd2ba797b066898704ec687407a6a9972020d1f1
James Bowman <jamesb@excamera.com>
2017-07-14 20:17:39 -0700

    Floorplan renderer as command-line utility

---
+++ src/render.py
@@ -0,0 +1,9 @@
+import sys
+from ga144 import GA144
+import draw
+
+if __name__ == '__main__':
+    g = GA144()
+    g.loadprogram(sys.argv[1])
+    v = draw.Viz(g.active())
+    v.render("pictures/%s.png" % sys.argv[1])
